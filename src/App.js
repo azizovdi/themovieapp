22935fccce9bbbae7914b79543e4b0d0dab2f2c2
Azizov Diyorbek <azizovdiyorbek@gmail.com>
2022-07-06 04:45:17 +0500

    Finished the hero section, and fetched moviedb API

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { GlobalStyle } from "./GlobalStyle";
+
+const App = () => (
+  <div>
+    <Header />
+    <Home />
+    <GlobalStyle />
+  </div>
+);
+
+export default App;
