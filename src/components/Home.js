6f44181bb729effe72536420b5929993d56ab6a2
Azizov Diyorbek <azizovdiyorbek@gmail.com>
2022-07-06 05:23:59 +0500

    Added Grid component with popular movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
 //components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
@@ -24,6 +27,24 @@ const Home = () => {
           text={state.results[0].overview}
         />
       )}
+
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          >
+            {movie.title}
+          </Thumb>
+        ))}
+      </Grid>
+      <Spinner />
     </>
   );
 };
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect, useRef} from "react";
+
+import { Wrapper, Content } from "./SearchBar.styles";
+
+const SearchBar = () => (
+
+)
